2bfda3c2d56bcb370dc74ef4cb270fb1fead1169
Clément HUGON <Clement.HUGON@diiage.org>
2021-04-20 15:14:09 +0200

    Create Main page with design

---
+++ src/main/java/org/diiage/clementh/poc/hugon/swapi/PeopleActivity.java
@@ -0,0 +1,14 @@
+package org.diiage.clementh.poc.hugon.swapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people);
+    }
+}
